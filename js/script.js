91b0f2278b9e74dcadd727c976f3193b95d63802
mehmet <tas349873@gmail.com>
2022-11-06 16:17:11 +0300

    update

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.onscroll = () =>{
+    if(window.scrollY >80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.add('active');
+    }
+   
+}
