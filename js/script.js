e12b9217d7e406c7aa1f2cad010036f9492b7f0a
mehmet <tas349873@gmail.com>
2022-11-06 19:17:24 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
+searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+  searchForm.classList.toggle('active');
+}
+
+
 window.onscroll = () =>{
     if(window.scrollY >80){
         document.querySelector('.header .header-2').classList.add('active');
     }else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+   
+}
+
+
+window.onload = () =>{
+    if(window.scrollY >80){
         document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
     }
    
 }
